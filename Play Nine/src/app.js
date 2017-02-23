2e9d7ebef2dff56993b4a273e0df749ca07f183c
shahenkosyan <shahen.qosyan@gmail.com>
2017-02-23 23:35:33 +0400

    Initialized with npm

---
+++ Play Nine/src/app.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Shahen Kosyan on 2/23/17.
+ */
+
+'use strict';
+
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <div className="app-header">
+          <h2>Welcome to React</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Play Nine/src/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Shahen Kosyan on 2/23/17.
+ */
+
+
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
